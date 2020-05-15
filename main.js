8ecbc6628eeadec8549e5f607c5d5864d0e6ac5a
Ramon Audain <raudain123@gmail.com>
2020-05-15 18:08:20 +0000

    Solution #2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Write a function that accpets a positve numer N.
+// Write a function that accepts a positve numer N.
 // The function should console log a step shape with N levels using the # character.
 // Make sure the step has spaces on the right hand size!
 
@@ -11,6 +11,24 @@ function steps(n){
     console.log("#".repeat(count));
     count++;
   }
+
+
+  // Solution #2
+  for (let row = 0; row < n; row++){
+    let stair = "";
+
+    for (let column = 0; column < n; column++){
+      if (column <= row){
+        stair += "#";
+      }
+      else{
+        stair += " ";
+      }
+    }
+
+  console.log(stair);
+  }
+ 
 }
 
-steps(50);
+steps(10);
